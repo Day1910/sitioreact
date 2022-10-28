6edd5f654f8a8d3f1b42394438d33e83d32cc558
Daytiana Ramirez Huayhua <daytiana.ramirez.huayhua@gmail.com>
2022-10-27 19:28:12 -0500

    V02  my-app1 Proyecto React Se modifico el nombre

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - Daytiana
+          Learn React - Day
         </a>
       </header>
     </div>
