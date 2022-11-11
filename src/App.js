abe9b7e322d2e166d3c71b84a3c234e067c48f65
Daytiana Ramirez Huayhua <daytiana.ramirez.huayhua@gmail.com>
2022-11-10 20:33:45 -0500

    V01 REGISTRO EXAMEN

--- src/App.js
+++ src/App.js
@@ -1,29 +1,64 @@
-import { useState } from "react";
+import {collection, getDocs, query, doc, deleteDoc, where,} from "firebase/firestore";
+
+import React, { useEffect, useState } from "react";
+import firebase, { db } from "./Componentes/firebase";
 import AppForm from "./Componentes/AppForm";
 
 
+
 function App() {
-////////////////////////Definir lectura/////////////////////////
-const [idActual, setIdActual] = useState("");    ////PARA actuañizar y eliminar
-const [docsBD, setDocsBD] = useState([]);        ////PARA LECTURA A BD
+    ////////////////////////Definir lectura/////////////////////////
+    const [idActual, setIdActual] = useState("");    ////PARA actuañizar y eliminar
+    const [docsBD, setDocsBD] = useState([]);        ////PARA LECTURA A BD
+    const [orden, setOrden] = useState(0); 
+    const i = 1; 
 
-//////////////LECTURA A BD/////////////////////////////////
-const fnRead = () =>{
-  console.log("Lectura a BD");
-}
+    //////////////LECTURA A BD/////////////////////////////////
+    const fnRead = async () =>{
+      const tblPersona = query(collection(db, "favoritos"), where ("URL","!=", ""));
+      const xDatosBD = await getDocs(tblPersona);
+      const xDoc = [];
 
-//////////////ELIMINAR/////////////////////////////////
-const fnDelete = () =>{
-  console.log("Eliminar un registro");
-}
+      xDatosBD.forEach((doc) => {
+        xDoc.push({id: doc.id, ...doc.data()});
+      });
+      setDocsBD(xDoc);
+      //console.log("Lectura a BD");
+    }
+
+    useEffect( () => {
+      fnRead();
+    }, [idActual]);
+
+    //////////////ELIMINAR/////////////////////////////////
+    const fnDelete = async (xId) =>{
+      if (window.corfirm("Confirme para eliminar")) {
+        await deleteDoc(doc(db, 'favoritos', xId));
+        console.log("Se elimino ...."+ xId);
+      }
+      fnRead();
+
+    }
 
 
-  return (
-    <div style={{background:"greenyellow", width:"350px"}}>
-      <h1> sitiocopia (App.js)</h1>
-      <AppForm {...{idActual, setIdActual, fnRead}}/>
-    </div>
-  );
+      return (
+        <div style={{background:"greenyellow", width:"350px", padding:"10px"}}>
+          <h1> sitiocopia2 (App.js)</h1>
+          <AppForm {...{idActual, setIdActual, fnRead}}/>
+         {
+          docsBD.map( (p) =>
+            <p key={p.xId}>
+                N.{i} - {p.URL} ---
+                <span onClick={() => fnDelete(p.xId)}>x</span>
+                ---
+                <span onClick={() => setIdActual(p.xId)}>A</span>
+                ---
+            </p>
+          )
+         }
+         
+        </div>
+      );
 }
 
 export default App;
--- src/Componentes/AppForm.js
+++ src/Componentes/AppForm.js
@@ -1,24 +1,88 @@
-import React from 'react'
+import {collection, doc, getDocs, addDoc, updateDoc} from "firebase/firestore";
+
+import React, { useEffect, useState } from "react";
+import firebase,{db} from "./firebase";
+
 
 
 const AppForm = (props) => {
 
-const camposRegistro = {nombre:'', edad:'', genero:''}
-const [objeto, setObjeto] = useState(camposRegistro);
+    const camposRegistro = {URL:'', nombre:'', descripcion:''}
+    const [objeto, setObjeto] = useState(camposRegistro);
 
-const controlarEstadoCambio = (e) => {
-     
-};
+    const controlarEstadoCambio = (e) => {
+        const{URL, value} = e.target;
+        setObjeto({...objeto, [URL]:value })
 
-const controlSubmit= (e) => {
-     
-};
+    }
 
-  return (
-    <div style ={{background:"orange"}}>
-        Formulario AppForm.js
-    </div>
-  )
+    const controlSubmit = async (e) => {
+        e.preventDefault();
+
+        if(props.idActual === ""){
+            if (validarForm) {
+              addDoc(collection(db, 'favoritos'), objeto);
+              console.log("Se guardo...");
+              props.fnRead();
+            }else{
+              console.log("No se guardo...");
+            }
+        }else{
+            await updateDoc(doc(collection(db, "favoritos"), props.idActual),objeto);
+            console.log("Se actualizo...");
+            props.fnRead();
+            props.setIdActual('');
+        }
+        setObjeto(camposRegistro);
+    }
+
+    const validarForm = () => {
+        if (objeto.URL==="" || /^\s+$/.test(objeto.URL)) {
+            alert("Escriba URL...");
+            return false;
+        }
+        return true;
+    };
+
+    useEffect( () => {
+        if (props.idActual === "") {
+          setObjeto({...camposRegistro});
+        } else {
+          obtenerDatosPorId(props.idActual);
+        }
+        props.fnRead();
+    }, [props.idActual]);
+
+    const obtenerDatosPorId = async(xId) => {
+        const objPorId = doc(db, "favoritos", xId);
+        const docPorId = await getDocs(objPorId);
+        if (docPorId.exists()) {
+          setObjeto(docPorId.data());
+        } else {
+          console.log("No hay doc...");
+        }
+    };
+
+      return (
+        <div style ={{background:"orange", padding:"10px", textAlign:"center"}}>
+            <h1>AppForm.js </h1>
+            <form onSubmit={controlSubmit}>
+              <input type="text" name="URL" placeholder="URL.." onChange={controlarEstadoCambio}
+              value={objeto.nombre}/> <br/>
+
+              <input type="text" name="nombre" placeholder="Nombre.." onChange={controlarEstadoCambio}
+              value={objeto.edad}/> <br/>
+
+              <input type="text" name="descripcion" placeholder="Descripcion.." onChange={controlarEstadoCambio}
+              value={objeto.genero}/> <br/>
+
+              <button>
+                {props.idActual === ""? "Guardar" : "Actualizar"}
+              </button>
+
+            </form>
+        </div>
+      )
 }
 
 export default AppForm
